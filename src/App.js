f545f7c397a3bacf4ba0847fc162c83095430a3b
Luka Koridze <luka.qoridze2002@gmail.com>
2022-06-27 12:13:13 +0400

    responsive update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Galaxy from './Galaxy.js'
 function App() {
   const [launch,setLaunch] = useState(false)
   return (
-    <div className="App" style={{width: '100%', maxWidth: '1920px', margin: '0 auto'}}>
+    <div className="App" style={{width: '100%', maxWidth: '1920px', margin: '0 auto', height:'100vh', margin:'0 auto',boxSizing: 'border-box',padding:'0'}}>
       {!launch && <Start onClick={()=>{setLaunch(!launch)}}/>}
       {launch && <Galaxy/>}
     </div>
--- src/Galaxy.js
+++ src/Galaxy.js
@@ -4,12 +4,12 @@ import "./Galaxy.css"
 
 function Galaxy() {
   return (
-    <div className="galaxy centerContent">
+    <div className="galaxy centerContent" style={{width: '100%', height: '99.7vh', margin:'0 auto', maxWidth:'1920px', boxSizing: 'border-box',padding: '0px'}}>
       <div className="typing-demo1"> Planet: Earth </div>
       <div className="typing-demo2"> Location: Tbilisi, Georgia <i class="flag flag-georgia"></i></div>
       <div className="typing-demo3"> Date: 13 April, 2002</div>
       <div className="typing-demo4"> Event: Luka Koridze's birth</div>
-      <div className="twinkling"></div>
+      <div className="twinkling" style={{width: '100%', height: '99.7vh', margin:'0 auto', maxWidth:'1920px',boxSizing: 'border-box'}}></div>
       <div className="earth">
         <ReactEarth width="100%" height="100%" speed={0.5} />
       </div>
